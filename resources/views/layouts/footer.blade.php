7d02a4bb18f6fbe91a72861ea4f1ced83100d7cc
Gogo08 <gogo08190@gmail.com>
2021-05-26 11:48:10 +0200

    Page Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,22 @@
 <footer id="page-footer">
+  <section id="prefooter">
+    <div class="uk-container uk-container-xsmall uk-text-center">
+      <div class="social uk-grid-match uk-child-width-1-6@m uk-child-width-1-3@s" data-uk-grid>
+        <div><a href="{!! get_option('th_youtube') !!}" target="_blank" class="social_icon youtube"><i class="fab fa-youtube"></i></a></div>
+        <div><a href="{!! get_option('th_twitch') !!}" target="_blank" class="social_icon twitch"><i class="fab fa-twitch"></i></a></div>
+        <div><a href="{!! get_option('th_discord') !!}" target="_blank" class="social_icon discord"><i class="fab fa-discord"></i></a></div>
+        <div><a href="{!! get_option('th_twitter') !!}" target="_blank" class="social_icon twitter"><i class="fab fa-twitter"></i></a></div>
+        <div><a href="{!! get_option('th_instagram') !!}" target="_blank" class="social_icon instagram"><i class="fab fa-instagram"></i></a></div>
+        <div><a href="{!! get_option('th_github') !!}" target="_blank" class="social_icon github"><i class="fab fa-github"></i></a></div>
+      </div>
+    </div>
+  </section>
+  <section id="copyright">
+    <div class="uk-container">
+      <div class="uk-text-center">
+        <p>© <?php echo date ('Y'); ?> – {!! get_option('th_titre') !!} par <a href="mailto:contact@gogo08190.fr" rel="nofollow" title="Gogo08" target="_blank">Gogo08</a>. Tous droits réservés. <a href="/mentions-legales" rel="nofollow">Mentions légales.</a></p>
+      </div>
+    </div>
+  </section>
 
 </footer>
