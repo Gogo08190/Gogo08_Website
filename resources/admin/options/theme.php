add4a14a7129159b85ae7186fa51595a50e34e62
Gogo08 <gogo08190@gmail.com>
2021-05-31 08:32:31 +0200

    Backoffice

--- resources/admin/options/theme.php
+++ resources/admin/options/theme.php
@@ -4,6 +4,15 @@
  * Define your theme custom code.
 */
 
+/* Support thumbnails sur toutes les pages */
+add_theme_support('post-thumbnails');
+
+/* Suppression des pages inutiles sur le menu lateral */
+add_action('admin_menu', 'wpc_admin_menu' );
+function wpc_admin_menu(){
+  remove_menu_page("edit-comments.php");
+}
+
 add_action( 'wp_before_admin_bar_render', 'admin_bar_links' );
 function admin_bar_links() {
 	global $wp_admin_bar;
@@ -18,5 +27,11 @@ function admin_bar_links() {
 	$wp_admin_bar->remove_menu('revisr');      		// Remove revisr
 	$wp_admin_bar->remove_menu('view');
 }
+
+function remove_footer_admin () {
+	echo "<p>Contactez <a href='https://www.gogo08.fr' target='_blank'>Gogo08</a> si vous avez besoin d'aide par <a href='mailto:contact@gogo08190.fr'>mail</a></p>";
+}
+
+add_filter('admin_footer_text', 'remove_footer_admin');
 
 ?>
