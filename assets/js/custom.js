d7b081add0a3c3a28cb4c70510c607377f04f0af
Gogo08 <gogo08190@gmail.com>
2021-06-01 14:20:09 +0200

    Fix opera + ME

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,47 +13,6 @@ $(document).ready(function() {
         scaleY: 1
     };
 
-    if ($(window).width() > 960) {
-      $(function() {
-          var $page = $('#wrapper');
-
-          getPageSize();
-          scalePages($page, pageWidth, pageHeight, );
-          ratio = $('body').width() / $('body').height();
-          h = 1920 / ratio;
-
-          //using underscore to delay resize method till finished resizing window
-          $(window).resize(_.debounce(function () {
-              if ($(window).width() > 960) {
-                  getPageSize();
-                  scalePages($page, pageWidth, pageHeight);
-              }
-          }, 150));
-
-
-          function getPageSize() {
-              pageHeight = $('body').height();
-              pageWidth = $('body').width();
-              ratio = $('body').width() / $('body').height();
-              h = 1920 / ratio;
-          }
-
-          function scalePages(page, maxWidth, maxHeight) {
-              var scaleX = 1, scaleY = 1;
-              scaleX = maxWidth / basePage.width;
-              scaleY = maxHeight / basePage.height;
-              basePage.scaleX = scaleX;
-              basePage.scaleY = scaleY;
-              basePage.scale = (scaleX > scaleY) ? scaleY : scaleX;
-
-              var newLeftPos = Math.abs(Math.floor(((basePage.width * basePage.scale) - maxWidth)/2));
-              var newTopPos = Math.abs(Math.floor(((basePage.height * basePage.scale) - maxHeight)/2));
-
-              page.attr('style', 'transform:scale(' + basePage.scale + '); -webkit-transform:scale(' + basePage.scale + ');');
-          }
-      });
-    }
-
     Pace.on('hide', function() {
       wow = new WOW({
         boxClass: 'wow',
