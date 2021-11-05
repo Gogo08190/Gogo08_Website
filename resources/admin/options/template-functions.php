a0d36e0d8f98de07f216d2cb3b17526c8069471d
Gogo08 <gogo08190@gmail.com>
2021-11-05 22:22:12 +0100

    Add auto safe post / draft

--- resources/admin/options/template-functions.php
+++ resources/admin/options/template-functions.php
@@ -25,9 +25,70 @@ Filter::add('body_class', function ($classes) {
 });
 
 /**
+ * Add function save post / draft
+*/
+
+add_action("save_post", "save_metabox");
+add_action("save_draft", "save_metabox");
+function save_metabox() {
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		return $post_id;
+	} else {
+		$options = array();
+
+		foreach ($_POST as $key => $value) {
+			if(substr($key, 0, 3) === 'th_') {
+				$options[] = $key;
+			}
+		}
+
+		foreach ($options as $O) {
+			update_post_meta(get_the_ID(), $O, $_POST[$O]);
+		}
+	}
+}
+
+/**
+ * Add function autosave
+*/
+
+add_action('admin_footer', 'admin_custom_script');
+function admin_custom_script() {
+  echo '<script>
+          jQuery(document).ready(function($) {
+          $(document).on("click", ".themosis__tabs__menu button, .supports-drag-drop, .themosis__collection__button--remove, .themosis__input__radio", function() {
+            $(".themosis__metabox__footer button").click();
+          });
+
+          $(document).on("mouseup", ".themosis__collection__item.selected", function() {
+            $(".themosis__metabox__footer button").click();
+          });
+
+          $(document).on("touchstart", function(e) {
+            if(($(e.target).text() == "Insert") || ($(e.target).text() == "Remove Selected")) {
+              setTimeout(
+                function() {
+                  $(".themosis__metabox__footer button").click();
+                }, 500);
+            }
+          });
+        });
+  </script>';
+}
+
+/**
+ * Remove save button metabox themosis
+*/
+
+add_action('admin_footer', 'admin_custom_footer');
+function admin_custom_footer() {
+	echo '<style>.themosis__metabox__footer button { display : none !important; }</style>';
+}
+
+/**
  * Remove head and footer links
  */
- 
+
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_generator');
@@ -52,9 +113,9 @@ function disable_emojis() {
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
 	add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
@@ -65,7 +126,7 @@ function disable_emojis_tinymce( $plugins ) {
 	if ( is_array( $plugins ) ) {
 		return array_diff( $plugins, array( 'wpemoji' ) );
 	}
-	
+
 	else {
 		return array();
 	}
--- resources/admin/options/theme.php
+++ resources/admin/options/theme.php
@@ -28,10 +28,52 @@ function admin_bar_links() {
 	$wp_admin_bar->remove_menu('view');
 }
 
+if(!is_admin()) {
+  add_action(
+    'after_setup_theme',
+    function() {
+      add_theme_support('html5', ['script', 'style']);
+    }
+  );
+
+  add_action('wp_loaded', 'output_buffer_start');
+  function output_buffer_start() {
+    ob_start("output_callback");
+  }
+
+  function output_callback($buffer) {
+    return preg_replace( "%[ ]type=[\'\"]text\/(javascript|css)[\'\"]%", '', $buffer );
+  }
+
+  add_filter('style_loader_tag', 'optimize_style_tags', 10, 4);
+  function optimize_style_tags($html, $handle, $href, $media) {
+    return '<link rel="stylesheet" href="' . $href . '" media="' . $media . '">' . "\n";
+  }
+
+  add_filter('script_loader_tag', 'optimize_script_tags', 10, 3);
+  function optimize_script_tags($tag, $handle, $src) {
+    return '<script src="' . $src . '"></script>' . "\n";
+  }
+}
+
 function remove_footer_admin () {
 	echo "<p>Contactez <a href='https://www.gogo08.fr' target='_blank'>Gogo08</a> si vous avez besoin d'aide par <a href='mailto:contact@gogo08190.fr'>mail</a></p>";
 }
 
 add_filter('admin_footer_text', 'remove_footer_admin');
+
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
+// remove jquery
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+   wp_deregister_script('jquery');
+}
 
 ?>
