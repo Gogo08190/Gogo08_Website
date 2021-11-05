e5d2773a6b75d965a73b16da26527a04c2e36c96
Gogo08 <gogo08190@gmail.com>
2021-11-05 22:59:03 +0100

    Share buttons

--- resources/views/actus/single.blade.php
+++ resources/views/actus/single.blade.php
@@ -15,5 +15,21 @@
 		<div class="uk-container texte">
 			{!! Loop::content() !!}
 		</div>
+
+    <div class="uk-container share uk-text-center">
+      <?php
+      /* Social Share Buttons template for Wordpress
+       * By Daan van den Bergh
+       */
+
+      $postUrl = 'http' . ( isset( $_SERVER['HTTPS'] ) ? 's' : '' ) . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"; ?>
+
+      <div class="share-button-wrapper">
+        <span>Partager</span>
+        <a target="_blank" class="share-button share-twitter" href="https://twitter.com/intent/tweet?url=<?php echo $postUrl; ?>&text=<?php echo the_title(); ?>&via=<?php the_author_meta( 'twitter' ); ?>" title="Tweet this"><i class="fab fa-twitter"></i></a>
+        <a target="_blank" class="share-button share-facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $postUrl; ?>" title="Share on Facebook"><i class="fab fa-facebook"></i></a>
+      </div>
+    </div>
+
 	</div>
 @endsection
